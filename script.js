bca8728bcb33466894a2dc95c695fbcba7115784
Michael Thede <michael.r.thede@gmail.com>
2025-08-11 15:52:26 -0400

    added hover -> color change to small divs

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const grid = document.querySelector("#grid");
 const width = window.innerWidth / 16;
 const height = window.innerHeight / 16;
+const permanent = "width: " + width + "px; height: " + height + "px;"
 let id = 0;
 let innerID = 0;
+
+function highlight(element){
+    element.setAttribute("style", "background-color: blue; " + permanent);
+}
+
+function lowlight(element){
+    element.setAttribute("style", "background-color: white; " + permanent);
+}
+
 for(let i = 0; i < 16; i++){
     const div = document.createElement("div");
     div.setAttribute("id", "div" + id);
@@ -11,8 +21,9 @@ for(let i = 0; i < 16; i++){
         const innerDiv = document.createElement("div");
         innerDiv.setAttribute("id", "innerDiv" + innerID);
         innerDiv.setAttribute("class", "innerDiv");
-        innerDiv.setAttribute("style", "width: " + width + "px; height: " + height + "px;");
-
+        innerDiv.setAttribute("style", permanent);
+        innerDiv.addEventListener("mouseenter", () => highlight(innerDiv));
+        innerDiv.addEventListener("mouseleave", () => lowlight(innerDiv));
         div.appendChild(innerDiv);
         innerID++;
     }
