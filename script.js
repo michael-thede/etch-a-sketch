49c718b07797f980d517e33580db891e3290723c
Michael Thede <michael.r.thede@gmail.com>
2025-08-11 15:02:10 -0400

    Added inner divs for each outer div

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const grid = document.querySelector("#grid");
 let id = 0;
+let innerID = 0;
 for(let i = 0; i < 16; i++){
     const div = document.createElement("div");
     div.setAttribute("id", "div" + id);
     for(let j = 0; j < 16; j++){
-
+        const innerDiv = document.createElement("div");
+        innerDiv.setAttribute("id", "innerDiv" + innerID)
+        div.appendChild(innerDiv);
+        innerID++;
     }
+    innerID = 0;
     id++;
     grid.appendChild(div);
 }
